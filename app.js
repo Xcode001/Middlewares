33091eb626a9ff02a9f3f519be30fe5df7ee5441
Musa Musa <musamusa739@gmail.com>
2018-10-23 10:53:51 +0100

    Adding Middlewares using functional approach

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express'),
           app = express();
 
-// Using a general Middleware with 'use' method
-app.use((req, res, next)=>{
-  if(0 > 1){
-    res.json("This is incorrect");
-  }else {
-    next(); //Proceeds to the next route
+//Using a function as a middleware
+function enteringAnotherState(req, res, next){
+  if(req.params.abuja === 'abuja'){
+    next();
+  }else{
+    res.json("This wont work");
   }
-});
+}
+
+app.get("/way-to/:abuja", enteringAnotherState, (req, res)=>{
+    res.json("Welcome to "+ req.params.abuja)
+})
 
 app.get("/", (req, res)=>{
   res.json("Welcome to the development world")
